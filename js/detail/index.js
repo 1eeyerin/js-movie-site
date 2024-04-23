b9e2120df961ced6f8fa675f65f85daee91e2e16
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-04-23 23:40:01 +0900

    feat: 영화 상세페이지 구현 목업 추가

--- js/detail/index.js
+++ js/detail/index.js
@@ -1,12 +1,16 @@
 import includeHTML from "../utils/includeHTML.js";
 import common from "../common.js";
+import {getMovieDetail} from "../api/getMovieDetail.js";
+import customCursor from "../customCursor.js";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", (() => {
   includeHTML(() => {
     common();
     onGoBack();
+    getMovieDetail();
+    customCursor();
   });
-});
+}));
 
 const onGoBack = () => {
   const button = document.querySelector('#goBackBtn');
