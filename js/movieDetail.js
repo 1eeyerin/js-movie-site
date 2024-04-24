23d47b6ffbcf5d890d08168c8f158d72398789ef
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-04-24 13:48:51 +0900

    feat: promise를 이용하여 movie-detail 동기 실행 보장

--- js/movieDetail.js
+++ js/movieDetail.js
@@ -1,29 +1,63 @@
 import {IMG_PATH} from "./api/constants";
 
-const createMovieDetail = (movie) => {
-  const listItem = document.createElement('li');
-  listItem.classList.add('contents');
+const insertMovieDetail = ({ selector, data }) => {
+  return new Promise((resolve) => {
+    const fragment = new DocumentFragment();
+    const wrapper = document.querySelector(selector);
 
-  const link = document.createElement('a');
-  link.href = `./detail.html?movieId=${movie.id}`;
+    const featureSection = document.createElement('section');
+    const topInfoSection = document.createElement('section');
+    featureSection.classList.add('feature-section');
+    topInfoSection.classList.add('top-info-section');
 
-  const image = document.createElement('img');
-  image.src = `${IMG_PATH}${movie['poster_path']}`;
-  image.alt = '';
+    const bg = document.createElement('div');
+    bg.classList.add('detail-bg');
+    bg.style.backgroundImage = `url('${IMG_PATH}${data.poster_path}')`;
+    featureSection.appendChild(bg);
 
-  const movieInfo = document.createElement('div');
-  movieInfo.classList.add('movie-info');
+    const thumbnailImg = document.createElement('img');
+    thumbnailImg.classList.add('detail-thumbnail', 'custom_mousemove');
+    thumbnailImg.src = `${IMG_PATH}${data.poster_path}`;
+    topInfoSection.appendChild(thumbnailImg);
 
-  const title = document.createElement('strong');
-  title.classList.add('movie-info-title');
-  title.textContent = movie.title?.length > 15 ? movie.title.slice(0, 15) + '...' : movie.title;
+    const title = document.createElement('h2');
+    title.classList.add('detail-title');
+    title.textContent = data.title;
+    topInfoSection.appendChild(title);
 
-  movieInfo.appendChild(title);
-  link.appendChild(image);
-  link.appendChild(movieInfo);
-  listItem.appendChild(link);
+    const date = document.createElement('div');
+    date.classList.add('detail-date');
+    date.textContent = `(${data.release_date.slice(0, 4)})`;
+    topInfoSection.appendChild(date);
 
-  return listItem;
-}
+    const summary = document.createElement('p');
+    summary.classList.add('detail-summary');
+    summary.textContent = data.overview;
+    topInfoSection.appendChild(summary);
 
-export {createMovieDetail}
+    const bottomInformation = document.createElement('ul');
+    bottomInformation.classList.add('detail-bottom-information');
+
+    const infoItems = [
+      `평점 : ${data.vote_average}`,
+      `개봉일 : ${data.release_date}`,
+      `장르 : ${data.genres.map(({name}) => name).join(', ')}`,
+      `상영 시간 : ${data.runtime}분`
+    ];
+
+    infoItems.forEach(itemText => {
+      const listItem = document.createElement('li');
+      listItem.textContent = itemText;
+      bottomInformation.appendChild(listItem);
+    });
+
+    topInfoSection.appendChild(bottomInformation);
+    fragment.appendChild(featureSection).appendChild(topInfoSection);
+
+    wrapper.appendChild(fragment);
+
+    resolve(true);
+  });
+};
+
+export { insertMovieDetail };
