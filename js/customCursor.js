afa6fa06dbb105eb2ecac23198878f3f6335caf8
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-04-24 23:51:59 +0900

    feat: 미디어쿼리 css / customCusor 1200 너비 이하에서는 동작 안하도록 설정

--- js/customCursor.js
+++ js/customCursor.js
@@ -1,23 +1,17 @@
 const customCursor = () => {
-  const cursor_primary = document.getElementById('custom_cursor');
-  const circle = cursor_primary.querySelector('.custom_cursor_circle');
+  if (window.innerWidth < 1200) return;
+
+  const cursorPrimary = document.getElementById('custom_cursor');
+  const circle = cursorPrimary.querySelector('.custom_cursor_circle');
 
   document.addEventListener('mousemove', (e) => {
-    gsap.to(cursor_primary, { duration: 0.7, opacity: 1, x: e.clientX, y: e.clientY, ease: 'power3.out' });
+    gsap.to(cursorPrimary, { duration: 0.7, opacity: 1, x: e.clientX, y: e.clientY, ease: 'power3.out' });
   });
 
   const mouseEnterHandler = (event) => {
     event.target.classList.add('move_on');
-    const size = event.target.dataset.size !== undefined ? event.target.dataset.size : '100%';
-
-    if (event.target.classList.contains('small_h')) {
-      cursor_primary.classList.add('small');
-    } else {
-      cursor_primary.classList.remove('small');
-    }
-
     gsap.killTweensOf(circle);
-    gsap.to(circle, { duration: 0.3, width: size, height: size, autoAlpha: 1, ease: 'power0.none' });
+    gsap.to(circle, { duration: 0.3, width: '100%', height: '100%', autoAlpha: 1, ease: 'power0.none' });
   };
 
   const mouseLeaveHandler = (event) => {
@@ -26,11 +20,9 @@ const customCursor = () => {
     event.target.classList.remove('move_on');
   };
 
-  const customMousemoveElements = document.querySelectorAll('.custom_mousemove');
-
-  customMousemoveElements.forEach((element) => {
-    element.addEventListener('mouseenter', mouseEnterHandler);
-    element.addEventListener('mouseleave', mouseLeaveHandler);
+  document.querySelectorAll('.custom_mousemove').forEach((el) => {
+    el.addEventListener('mouseenter', mouseEnterHandler);
+    el.addEventListener('mouseleave', mouseLeaveHandler);
   });
 }
 
