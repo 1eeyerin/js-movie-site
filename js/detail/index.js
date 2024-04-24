938b994c7dac14e9f876fac8704ae4ff521be959
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-04-24 23:29:33 +0900

    feat: animate 설정

--- js/detail/index.js
+++ js/detail/index.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", (() => {
     await getMovieDetail();
     await customCursor();
     await hideLoadingOverlay();
+    await setPageAnimate();
   });
 }));
 
@@ -19,3 +20,38 @@ const onGoBack = () => {
 
   button.onclick = () => window.history.back();
 };
+
+const setPageAnimate = () => {
+  const wrapElements = document.querySelector(".top-info-section");
+
+  if (!wrapElements) {
+    return false;
+  }
+
+  const titleElements = wrapElements.querySelectorAll(".detail-title span");
+  const dateElements = wrapElements.querySelector(".detail-date");
+  const summaryElements = wrapElements.querySelector(".detail-summary");
+  const informationElements = wrapElements.querySelector(".detail-bottom-information");
+
+  gsap
+    .timeline()
+    .staggerFromTo(
+      titleElements,
+      0.8,
+      {opacity: 0, y: '100%'},
+      {opacity: 1, y: '0%', delay: 0.2, ease: Power1.easeInOut},
+      0.05
+    )
+    .fromTo(
+      [summaryElements, dateElements, informationElements].filter(el => el),
+      {opacity: 0, y: '35px', filter: 'blur(10px)'},
+      {
+        opacity: 1,
+        y: '0',
+        filter: 'blur(0px)',
+        delay: 0,
+        ease: "power1.inOut",
+        duration: 0.8
+      }
+    );
+}
