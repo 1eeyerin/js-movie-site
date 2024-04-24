8b6410d10014d17ed726555962bfdcfe576b66ad
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-04-25 00:43:36 +0900

    refactor: includeHtml 리팩터링

--- js/utils/includeHTML.js
+++ js/utils/includeHTML.js
@@ -1,35 +1,25 @@
 const includeHTML = (callback) => {
-  var promises = [];
-
-  var elements = document.querySelectorAll('[include-html]');
-  elements.forEach(function(el) {
-    var file = el.getAttribute('include-html');
-    if (file) {
-      var promise = new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (this.readyState == 4) {
-            if (this.status == 200) {
-              el.innerHTML = this.responseText;
-              resolve();
-            } else {
-              reject(new Error('Failed to load: ' + file));
-            }
-          }
-        };
-        xhr.open("GET", file, true);
-        xhr.send();
-      });
-      promises.push(promise);
+  const elements = document.querySelectorAll('[include-html]');
+  const promises = [...elements].map(el => {
+    const file = el.getAttribute('include-html');
+    if (!file) {
+      return Promise.resolve(true);
     }
+
+    return fetch(file)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to load HTML: ${file}`);
+        }
+
+        return response.text();
+      })
+      .then(html => el.innerHTML = html);
   });
 
-  Promise.all(promises).then(function() {
-    callback();
-  }).catch(function(error) {
-    console.error(error);
-    callback(); // 호출에 실패하더라도 콜백은 호출되어야 함
-  });
-}
+  Promise.all(promises)
+    .catch(error => {console.error(error)})
+    .finally(() => callback())
+};
 
 export default includeHTML;
