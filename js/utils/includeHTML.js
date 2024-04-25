455015f2d777a5284af2c5a70967db996a90fd00
Yerin <yerinlee@iyelin-ui-MacBookPro.local>
2024-04-25 11:46:27 +0900

    feat: appendHtml export 함수 추가 후 getMovieDetail에서 error가 나면 not-found.html을 append 할 수 있도록 구현

--- js/utils/includeHTML.js
+++ js/utils/includeHTML.js
@@ -1,25 +1,40 @@
 const includeHTML = (callback) => {
   const elements = document.querySelectorAll('[include-html]');
   const promises = [...elements].map(el => {
-    const file = el.getAttribute('include-html');
-    if (!file) {
+    const filePath = el.getAttribute('include-html');
+    if (!filePath) {
       return Promise.resolve(true);
     }
 
-    return fetch(file)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Failed to load HTML: ${file}`);
-        }
-
-        return response.text();
-      })
+    return fetchForHtmlText(filePath)
       .then(html => el.innerHTML = html);
   });
 
   Promise.all(promises)
-    .catch(error => {console.error(error)})
+    .catch(error => console.error(error))
     .finally(() => callback())
 };
+
+const fetchForHtmlText = async (filePath) => {
+  return await fetch(filePath)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to load HTML: ${filePath}`);
+      }
+      return response.text();
+    });
+}
+
+export const appendHtml = async (selector, filePath) => {
+  const node = document.querySelector(selector);
+
+  await fetchForHtmlText(filePath)
+    .then(async html => {
+      const divElement = document.createElement('div');
+      divElement.innerHTML = await html;
+      node.appendChild(divElement);
+    })
+    .catch(error => console.error(error));
+}
 
 export default includeHTML;
